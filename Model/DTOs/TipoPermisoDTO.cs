833746f05eaac50531b0f58e51b48dbc6f293cbd
raldydeoleo@gmail.com <rdeoleo@brainsoft.com.do>
2020-11-28 23:39:31 -0400

    Primer commit

---
+++ Model/DTOs/TipoPermisoDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolicitudPermisos.Model.DTOs
+{
+    public class TipoPermisoDTO
+    {
+        [Required]
+        int TipoPermisoId { get; set; }
+
+        [Required]
+        [StringLength(30,
+        ErrorMessage = "Descripcion no debe tener mas de 30 caracteres.")]
+        string Descripcion { get; set; }
+    }
+}
